19ba3c66d1ac8d875ed116f2c0a1d62afc9bd1fa
Nico <nlewicki@student.42heilbronn.de>
2024-09-02 10:26:31 +0200

    added banner and basic interface

---
+++ include/minishell.h
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   minishell.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/09/02 09:08:07 by nlewicki          #+#    #+#             */
+/*   Updated: 2024/09/02 09:12:23 by nlewicki         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef MINISHELL_H
+# define MINISHELL_H
+
+# include <stdio.h>
+# include <stdlib.h>
+# include <readline/readline.h>
+# include <readline/history.h>
+# include <signal.h>
+# include <fcntl.h>
+# include <dirent.h>
+# include <unistd.h>
+# include <sys/types.h>
+# include <sys/wait.h>
+# include <sys/stat.h>
+
+#endif
