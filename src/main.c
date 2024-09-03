c545ef1f8f176e65340c790be6eef9d52198956a
Manuel <mhummel@student.42heilbronn.de>
2024-09-03 13:16:23 +0200

    fix

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 09:52:41 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/09/03 13:16:09 by mhummel          ###   ########.fr       */
+/*   Updated: 2024/09/03 13:16:16 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -129,9 +129,7 @@ int	execute_command(char *args[], int arg_count)
 	if (strcasecmp_custom(args[0], "unset") == 0)
 		return (ft_unset(args, arg_count));
 	if (strcasecmp_custom(args[0], "cd") == 0)
-	{
 		return (ft_cd(args, arg_count));
-	}
 	// Add other built-in commands here
 	// If not a built-in command, you can add logic to execute external commands
 	printf("Command not found: %s\n", args[0]);
