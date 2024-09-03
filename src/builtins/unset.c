f1f6d9e7385e9bf4a264731ba3af32df69708a0d
Manuel <mhummel@student.42heilbronn.de>
2024-09-03 13:17:53 +0200

    norminette fix

--- src/builtins/unset.c
+++ src/builtins/unset.c
@@ -3,57 +3,63 @@
 /*                                                        :::      ::::::::   */
 /*   unset.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/03 11:36:37 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/09/03 13:09:54 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/09/03 13:17:28 by mhummel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int is_valid_variable_name(const char *name)
+static int	is_valid_variable_name(const char *name)
 {
 	if (!isalpha(*name) && *name != '_')
-		return 0;
+		return (0);
 	while (*++name)
 	{
 		if (!isalnum(*name) && *name != '_')
-			return 0;
+			return (0);
 	}
-	return 1;
+	return (1);
 }
 
-static void remove_env_var(const char *name)
+static void	remove_env_var(const char *name)
 {
-	char **env = *env_vars();
-	int i, j;
+	char	**env;
+	int		i;
+	int		j;
+
+	env = *env_vars();
 	i = 0;
 	j = 0;
 	while (env[i])
 	{
 		j = 0;
-		if (strncmp(env[i], name, strlen(name)) == 0 && env[i][strlen(name)] == '=')
+		if (strncmp(env[i], name, strlen(name)) == 0
+			&& env[i][strlen(name)] == '=')
 		{
 			while (env[j])
 			{
 				env[j] = env[j + 1];
 				j++;
 			}
-			break;
+			break ;
 		}
 		i++;
 	}
 }
 
-int ft_unset(char *argv[], int argc)
+int	ft_unset(char *argv[], int argc)
 {
-	int i;
-	int status = 0;
+	int	i;
+	int	status;
+
+	status = 0;
 	if (argc < 2)
 	{
 		fprintf(stderr, "unset: not enough arguments\n");
-		return 1;
+		return (1);
 	}
 	i = 1;
 	while (i < argc)
@@ -69,5 +75,5 @@ int ft_unset(char *argv[], int argc)
 		}
 		i++;
 	}
-	return status;
+	return (status);
 }
