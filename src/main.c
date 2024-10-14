bf8e2fc5c30063829411df617f39207d95d2c396
Nico <nlewicki@student.42heilbronn.de>
2024-10-14 11:57:33 +0200

    added pipes

--- src/main.c
+++ src/main.c
@@ -6,19 +6,96 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 11:20:30 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/10/14 10:25:48 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/10/14 11:50:13 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int	g_signal = 0;
+int		g_signal = 0;
+
+int	create_child_process(t_list *tmp, int in_fd, int *pipefd)
+{
+	if (in_fd != 0)
+	{
+		dup2(in_fd, STDIN_FILENO);
+		close(in_fd);
+	}
+	if (tmp->next != NULL)
+	{
+		close(pipefd[0]);
+		dup2(pipefd[1], STDOUT_FILENO);
+		close(pipefd[1]);
+	}
+	execute_command(tmp);
+	exit(0);
+}
+
+void	execute_right_child(pid_t pid, int *in_fd, int *pipefd, t_list *tmp)
+{
+	waitpid(pid, NULL, 0);
+	if (*in_fd != 0)
+		close(*in_fd);
+	if (tmp->next != NULL)
+		close(pipefd[1]);
+	*in_fd = pipefd[0];
+}
+
+int	execute_pipe(t_list *tabel)
+{
+	t_list	*tmp;
+	int		pipefd[2];
+	pid_t	pid;
+	int		in_fd;
+
+	in_fd = 0;
+	tmp = tabel;
+	while (tmp != NULL)
+	{
+		if (tmp->next != NULL && pipe(pipefd) == -1)
+			return (write(2, "pipe failed\n", 12), 1);
+		pid = fork();
+		if (pid == -1)
+			return (write(2, "fork failed\n", 12), 1);
+		if (pid == 0)
+		{
+			if (create_child_process(tmp, in_fd, pipefd))
+				return (1);
+		}
+		else
+		{
+			execute_right_child(pid, &in_fd, pipefd, tmp);
+		}
+		tmp = tmp->next;
+	}
+	return (0);
+}
+
+int	execute_tabel(t_list *tabel)
+{
+	t_list		*tmp;
+	t_command	*cmd;
+
+	tmp = tabel;
+	cmd = (t_command *)tmp->content;
+	if (tmp != NULL)
+	{
+		if (tmp->next != NULL)
+		{
+			// write(1, "pipe\n\n", 5);
+			execute_pipe(tmp);
+		}
+		else if (cmd->args != NULL)
+			execute_command(tmp);
+	}
+	return (0);
+}
 
 void	main_loop(void)
 {
-	char *input;
-	t_list *tokens;
-	t_list *tabel;
+	char	*input;
+	t_list	*tokens;
+	t_list	*tabel;
 
 	while (1)
 	{
@@ -26,13 +103,13 @@ void	main_loop(void)
 		input = readline("minishell> ");
 		if (!input)
 			break ;
-
 		handle_history(input);
 		tokens = parse_input(input);
 		// print_token_list(tokens);
 		tabel = create_tabel(tokens);
 		// print_tabel(tabel);
-		execute_command(tabel); // add data?
+		execute_tabel(tabel);
+		// execute_command(tabel); // add data? ->we have exit_status() should do the same
 	}
 	printf("exit\n");
 }
@@ -52,11 +129,6 @@ int	main(int argc, char **argv, char **envp)
 	free_env(*env_vars());
 	return (*exit_status());
 }
-
-
-
-
-
 
 void	handle_shlvl(void)
 {
