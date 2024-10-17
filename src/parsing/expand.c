56545cb05fba97cc24b5f6af51524066901c61ab
Nico <nlewicki@student.42heilbronn.de>
2024-10-17 13:33:44 +0200

    expand.c

--- src/parsing/expand.c
+++ src/parsing/expand.c
@@ -6,7 +6,7 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:37:27 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/10/17 13:20:33 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/10/17 13:32:26 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -107,35 +107,70 @@ char	*expand_env_variables(char *src)
 	char	*result;
 	char	*start;
 	char	*end;
+	int		in_quotes;
 
 	result = NULL;
 	start = src;
 	end = src;
+	in_quotes = 0;
 	while (*end)
 	{
-		if (*end == '$' && (ft_isalnum(*(end + 1)) || *(end + 1) == '_'))
+		if (*end == '\'')
+			in_quotes = !in_quotes;
+		else if (*end == '$' && !in_quotes && (ft_isalnum(*(end + 1)) || *(end + 1) == '_'))
 		{
 			if (copy_until_dollar(&result, start, end) == NULL)
 				return (NULL);
 			if (handle_dollar(&result, &start, &end) == NULL)
 				return (NULL);
+			continue;
 		}
-		else
-			end++;
+		end++;
 	}
 	if (*start && copy_until_dollar(&result, start, end) == NULL)
 		return (NULL);
-	if (result == NULL)
-		result = ft_strdup(src);
-	return (result);
+	return (result ? result : ft_strdup(src));
 }
 
-void handle_expansion(t_list *tabel)
+static void	remove_empty_arg(t_command *row, size_t *i)
+{
+	size_t	j;
+
+	free(row->args[*i]);
+	j = *i;
+	while (row->args[j])
+	{
+		row->args[j] = row->args[j + 1];
+		j++;
+	}
+	(*i)--;
+}
+
+static void	process_arg(t_command *row, size_t *i)
+{
+	char	*expanded;
+
+	expanded = expand_env_variables(row->args[*i]);
+	if (expanded)
+	{
+		if (*expanded == '\0')
+		{
+			free(expanded);
+			remove_empty_arg(row, i);
+		}
+		else
+		{
+			free(row->args[*i]);
+			row->args[*i] = expanded;
+		}
+	}
+}
+
+void	handle_expansion(t_list *tabel)
 {
 	t_list		*tmp;
 	t_command	*row;
 	size_t		i;
-	char		*expanded;
 
 	tmp = tabel;
 	while (tmp)
@@ -146,14 +181,7 @@ void handle_expansion(t_list *tabel)
 		{
 			printf("row->args[%zu]: %s\n", i, row->args[i]);
 			if (ft_strchr(row->args[i], '$') != NULL)
-			{
-				expanded = expand_env_variables(row->args[i]);
-				if (expanded)
-				{
-					free(row->args[i]);
-					row->args[i] = expanded;
-				}
-			}
+				process_arg(row, &i);
 			i++;
 		}
 		tmp = tmp->next;
@@ -163,7 +191,6 @@ void handle_expansion(t_list *tabel)
 t_list	*expansion(t_list *tabel)
 {
 	handle_expansion(tabel);
-	// remove_quotes(tabel);
 	print_tabel(tabel);
 	return (NULL);
 }
