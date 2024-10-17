731633e1dd3efea5daee4a3fc92c4a82980d9a00
Nico <nlewicki@student.42heilbronn.de>
2024-10-17 12:42:42 +0200

    fixed ft_exit

--- src/execution/ft_exit.c
+++ src/execution/ft_exit.c
@@ -6,7 +6,7 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/17 10:38:59 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/10/17 10:39:14 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/10/17 12:42:34 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,7 @@ int	ft_exit(char *args[])
 		{
 			ft_putendl_fd("exit", 2);
 			ft_putendl_fd("minishell: exit: numeric argument required", 2);
-			exit(255);
+			*exit_status() = 255;
 		}
 		if (args[2])
 		{
@@ -34,7 +34,7 @@ int	ft_exit(char *args[])
 		exit_code = ft_atoi(args[1]);
 		exit_code = exit_code % 256;
 	}
-	*exit_status() = exit_code;
+	exit_code = *exit_status();
 	ft_putendl_fd("exit", 2);
 	exit(exit_code);
 }
