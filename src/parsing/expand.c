0685c377c5ed5c4255109d0104439290f547c317
Nico <nlewicki@student.42heilbronn.de>
2024-10-17 14:21:27 +0200

    added remove_quotes function

--- src/parsing/expand.c
+++ src/parsing/expand.c
@@ -6,7 +6,7 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/16 12:37:27 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/10/17 13:39:44 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/10/17 14:21:05 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -188,10 +188,103 @@ void	handle_expansion(t_list *tabel)
 	}
 }
 
+static void	shift_string(char *str, int start, int end)
+{
+	while (str[end])
+	{
+		str[start] = str[end];
+		start++;
+		end++;
+	}
+	str[start] = '\0';
+}
+
+static void	handle_double_quotes(char *str, int *i)
+{
+	int	start;
+	int	end;
+
+	start = *i;
+	end = start + 1;
+	while (str[end] && str[end] != '"')
+		end++;
+	if (str[end] == '"')
+	{
+		shift_string(str + start, 0, 1);
+		shift_string(str + end - 1, 0, 1);
+		*i = end - 2;
+	}
+	else
+		*i = end;
+}
+
+static void	handle_single_quotes(char *str, int *i)
+{
+	int	start;
+	int	end;
+	int	j;
+
+	start = *i;
+	end = start + 1;
+	while (str[end] && str[end] != '\'')
+		end++;
+	if (str[end] == '\'')
+	{
+		shift_string(str + start, 0, 1);
+		shift_string(str + end - 1, 0, 1);
+		j = start;
+		while (str[j] && j < end - 2)
+		{
+			if (str[j] == '\'')
+				shift_string(str + j, 0, 1);
+			else
+				j++;
+		}
+		*i = j - 1;
+	}
+	else
+		*i = end;
+}
+
+void	strip_quotes(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+	{
+		if (str[i] == '"')
+			handle_double_quotes(str, &i);
+		else if (str[i] == '\'')
+			handle_single_quotes(str, &i);
+		i++;
+	}
+}
+
+void	remove_quotes(t_list *tabel)
+{
+	t_list		*tmp;
+	t_command	*cmd;
+	size_t		i;
+
+	tmp = tabel;
+	while (tmp)
+	{
+		cmd = tmp->content;
+		i = 0;
+		while (cmd->args[i])
+		{
+			strip_quotes(cmd->args[i]);
+			i++;
+		}
+		tmp = tmp->next;
+	}
+}
+
 t_list	*expansion(t_list *tabel)
 {
-	handle_expansion(tabel);
-	// remove_quotes(tabel);
+	handle_expansion(tabel); // needs rework
+	remove_quotes(tabel); // seems to be working
 	print_tabel(tabel);
 	return (NULL);
 }
