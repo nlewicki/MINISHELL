6a796b74271212a96eab4e9e75bb934a58424454
Nico <nlewicki@student.42heilbronn.de>
2024-10-22 12:30:58 +0200

    norminette process token

--- Makefile
+++ Makefile
@@ -28,8 +28,6 @@ CFILES =	src/main.c \
 			src/parsing/create_tabel.c \
 			src/parsing/create_tabel2.c \
 			src/parsing/create_tabel3.c \
-			src/parsing/print_tabel.c \
-			src/parsing/print_list.c \
 			src/parsing/syntax_errors.c \
 			src/parsing/trim_len.c \
 			src/parsing/expand.c \
--- src/parsing/process_token.c
+++ src/parsing/process_token.c
@@ -6,7 +6,7 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/03 10:08:26 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/10/09 12:40:18 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/10/22 12:29:29 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,61 @@
 #include <limits.h>
 #include <sys/stat.h>
 
-bool	command_exists(const char *command) // prototype
+static char	*create_command_path(const char *path, const char *command)
 {
-	char *path_env;
-	char *path_copy;
-	char *path;
-	char command_path[PATH_MAX];
-	int written;
-	struct stat buffer;
+	char	*command_path;
+	size_t	path_len;
+	size_t	command_len;
+
+	path_len = ft_strlen(path);
+	command_len = ft_strlen(command);
+	command_path = malloc(path_len + command_len + 2);
+	if (!command_path)
+		return (NULL);
+	ft_strlcpy(command_path, path, path_len + 1);
+	command_path[path_len] = '/';
+	ft_strlcpy(command_path + path_len + 1, command, command_len + 1);
+	return (command_path);
+}
+
+static bool	check_command_exists(const char *command_path)
+{
+	struct stat	buffer;
+
+	if (stat(command_path, &buffer) == 0)
+	{
+		if (buffer.st_mode & S_IXUSR)
+			return (true);
+	}
+	return (false);
+}
+
+static bool	search_command_in_path(char *path_copy, const char *command)
+{
+	char	*path;
+	char	*command_path;
+	bool	exists;
+
+	path = ft_strtok(path_copy, ":");
+	while (path)
+	{
+		command_path = create_command_path(path, command);
+		if (!command_path)
+			return (false);
+		exists = check_command_exists(command_path);
+		free(command_path);
+		if (exists)
+			return (true);
+		path = ft_strtok(NULL, ":");
+	}
+	return (false);
+}
+
+bool	command_exists(const char *command)
+{
+	char	*path_env;
+	char	*path_copy;
+	bool	result;
 
 	path_env = get_our_env("PATH");
 	if (!path_env)
@@ -29,28 +76,9 @@ bool	command_exists(const char *command) // prototype
 	path_copy = ft_strdup(path_env);
 	if (!path_copy)
 		return (false);
-	path = ft_strtok(path_copy, ":");
-	while (path)
-	{
-		written = snprintf(command_path, sizeof(command_path), "%s/%s", path,
-				command);
-		if (written < 0 || (size_t)written >= sizeof(command_path))
-		{
-			path = ft_strtok(NULL, ":");
-			continue ;
-		}
-		if (stat(command_path, &buffer) == 0)
-		{
-			if (buffer.st_mode & S_IXUSR)
-			{
-				free(path_copy);
-				return (true);
-			}
-		}
-		path = ft_strtok(NULL, ":");
-	}
+	result = search_command_in_path(path_copy, command);
 	free(path_copy);
-	return (false);
+	return (result);
 }
 
 void	fill_struct(t_token *token, char *content)
