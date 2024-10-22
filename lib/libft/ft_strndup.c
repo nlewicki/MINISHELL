1045aa924f9287263bb737991f7ea7cc12294d47
Nico <nlewicki@student.42heilbronn.de>
2024-10-22 13:42:18 +0200

    ft_strndup, fix

--- lib/libft/ft_strndup.c
+++ lib/libft/ft_strndup.c
@@ -6,22 +6,32 @@
 /*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/12 13:24:17 by mhummel           #+#    #+#             */
-/*   Updated: 2024/10/22 13:18:53 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/10/22 13:41:54 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <string.h>
 
+static size_t	ft_strnlen(const char *s, size_t maxlen)
+{
+	size_t	len;
+
+	len = 0;
+	while (len < maxlen && s[len])
+		len++;
+	return (len);
+}
+
 char	*ft_strndup(const char *s, size_t n)
 {
 	char	*result;
 	size_t	len;
 
-	len = strnlen(s, n);
+	len = ft_strnlen(s, n);
 	result = (char *)malloc(len + 1);
 	if (!result)
 		return (NULL);
 	result[len] = '\0';
-	return ((char *)memcpy(result, s, len));
+	return ((char *)ft_memcpy(result, s, len));
 }
