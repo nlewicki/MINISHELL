943df2a30dd51bc0a76f16a3948ecb487b77afcf
Manuel <mhummel@student.42heilbronn.de>
2024-10-22 14:10:19 +0200

    Update minishell.h
    Sorted and norminette

--- include/minishell.h
+++ include/minishell.h
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   minishell.h                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
+/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/02 09:08:07 by nlewicki          #+#    #+#             */
-/*   Updated: 2024/10/22 13:37:52 by nlewicki         ###   ########.fr       */
+/*   Updated: 2024/10/22 14:06:25 by mhummel            #.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -96,190 +96,191 @@ typedef struct s_command
 	char			**red_symbol;
 }					t_command;
 
-//----- parsing -----
-// create_linked_list
+// Parsing
+
+// create_linked_list.c
 void				free_token(void *content);
 int					create_linked_list(char **tokens, t_list **list);
 
-// create_tabel
+// create_tabel.c
 t_info				*init_info(void);
 size_t				count_lines(t_list *tokens);
 void				process_command(t_info *info);
 t_list				*free_info_return_table(t_info *info);
 t_list				*create_tabel(t_list *tokens);
-// create_tabel2
+
+// create_tabel2.c
 t_command			*init_cmd(void);
 int					is_redirection_tabel(t_token *current_token);
 void				reset_info(t_info *info);
 void				count_words_redirections(t_list **tokens, t_info *info);
-// create_tabel3
+
+// create_tabel3.c
 t_command			*allocate_cmd(t_command *new_cmd, t_list *token_position,
 						t_info *info);
 void				process_redirection(t_command *cmd, t_list **tmp,
 						size_t *j);
-;
 void				process_argument(t_command *cmd, t_token *current_token,
 						size_t *i);
 t_command			*fill_cmd(t_command *cmd, t_list *position);
 
-// expand_env
-char				*expand_env_variables(char *src);
-// expand_process_arg
-void				process_arg(t_command *cmd, size_t *i);
-// expand_quotes
-void				remove_quotes(t_command *cmd);
-// expand
+// expand.c
 char				*get_our_env(const char *name);
 void				remove_empty_arg(t_command *row, size_t *i);
-;
 void				handle_expansion(t_command *cmd);
 t_list				*expansion(t_list *tabel);
-// expand2
+
+// expand2.c
 size_t				get_result_len(char *result);
 char				*copy_until_dollar(char **result, char *start, char *end);
-;
 char				*get_var_name(char **end);
 char				*append_var_value(char **result, char *var_value);
 char				*handle_dollar(char **result, char **start, char **end);
 
-// parsing_handler
-void				handle_non_specials(t_trim *trim, char *input);
-void				handle_operator(t_trim *trim, char *input);
-void				handle_quotes(t_trim *trim, char *input);
-void				handle_specials(t_trim *trim, char *input);
-// parsing
+// expand_env.c
+char				*expand_env_variables(char *src);
+
+// expand_process_arg.c
+void				process_arg(t_command *cmd, size_t *i);
+
+// expand_quotes.c
+void				remove_quotes(t_command *cmd);
+
+// parsing.c
 void				trim_str(t_trim *trim, char *input);
 char				*trim_whitespace(char *input);
 void				*check_syntax(char *input);
 t_list				*parse_input(char *input);
 
-// process_token
+// parsing_handler.c
+void				handle_non_specials(t_trim *trim, char *input);
+void				handle_operator(t_trim *trim, char *input);
+void				handle_quotes(t_trim *trim, char *input);
+void				handle_specials(t_trim *trim, char *input);
+
+// process_token.c
 bool				command_exists(const char *command);
 void				fill_struct(t_token *token, char *content);
 
-// split_quotes
+// split_quotes.c
 char				**split_space_quotes(const char *input);
 void				free_token_array(char **tokens);
-// split_quotes2
+
+// split_quotes2.c
 size_t				count_tokens(const char *str);
 char				*extract_token(const char *str, size_t *pos);
 
-// syntax_errors
+// syntax_errors.c
 char				*handle_syntax_errors(const char *input);
 
-// trim_len
+// trim_len.c
 int					ft_trim_len(char *input);
-// trim_len2
+
+// trim_len2.c
 bool				is_special_char(char c);
 
-//---- builtins ----
-// cd
-int			ft_cd(char *argv[], int argc);
+// Built-in Commands
 
-// echo
-int	ft_echo(char *argv[], int argc);
+// cd.c
+int					ft_cd(char *argv[], int argc);
 
-// env
-int		env(void);
-char	**copy_envp(char **envp);
-void	free_env(char **my_envp);
+// echo.c
+int					ft_echo(char *argv[], int argc);
 
-// export_utils
-int	is_valid_identifier(const char *str);
-char	**copy_env(char **envp, int count);
-int	count_env_vars(char **envp);
-void	bubble_sort_env(char **envp, int count);
-int	compare_env(const void *a, const void *b);
-// export_utils2
-int	set_export_error(char *name, char *value);
-int	add_or_update_env(char *name, char *value);
-char	*create_new_var(char *new_var, const char *name, char *value);
-int	add_new_env_var(const char *name, char *value, int i);
-// export
-int	ft_export(char **args, int arg_count);
-int ft_export_args(char *arg);
-int	mark_for_export(const char *name);
-int	print_sorted_env(void);
+// env.c
+int					env(void);
+char				**copy_envp(char **envp);
+void				free_env(char **my_envp);
 
-// pwd
-int	pwd(void);
+// export.c
+int					ft_export(char **args, int arg_count);
+int					ft_export_args(char *arg);
+int					mark_for_export(const char *name);
+int					print_sorted_env(void);
 
-// unset
-int	ft_unset(char *argv[], int argc);
+// export_utils.c
+int					is_valid_identifier(const char *str);
+char				**copy_env(char **envp, int count);
+int					count_env_vars(char **envp);
+void				bubble_sort_env(char **envp, int count);
+int					compare_env(const void *a, const void *b);
 
+// export_utils2.c
+int					set_export_error(char *name, char *value);
+int					add_or_update_env(char *name, char *value);
+char				*create_new_var(char *new_var, const char *name,
+						char *value);
+int					add_new_env_var(const char *name, char *value, int i);
 
+// pwd.c
+int					pwd(void);
 
+// unset.c
+int					ft_unset(char *argv[], int argc);
 
-// --- execution ---
-int					*is_expanded(void);
-char				*expand_env_variables(char *src);
-char				*handle_dollar(char **result, char **start, char **end);
-char				*copy_until_dollar(char **result, char *start, char *end);
-void				remove_quotes(t_command *cmd);
-int					ft_exit(char *args[]);
+// Execution
+
+// execute_command.c
 int					execute_command(t_list *tabel);
-void				handle_operator(t_trim *trim, char *input);
-void				handle_specials(t_trim *trim, char *input);
-void				fill_struct(t_token *token, char *content);
-int					create_linked_list(char **tokens, t_list **list);
-void				print_token_list(t_list *list);
-void				free_token(void *content);
-void				print_struct_tabel(t_list *tabel);
-t_list				*create_tabel(t_list *token_list);
-void				print_tabel(t_list *tabel);
-int					ft_exit(char *args[]);
-void				handle_shlvl(void);
-char				*expand_env_variables(char *src);
-int					ft_trim_len(char *input);
+void				exec_builtin(t_command *cmd, int builtin);
+int					is_builtin(t_command *cmd);
 
-// execute_external_utils
+// execute_external.c
+int					execute_external_command(char **args);
+
+// execute_external_utils.c
 int					handle_command_not_found(char **args);
 int					handle_parent_process(pid_t pid, char *command_path);
 void				ft_errorcode_exit(char *command, char *path);
 
-// fake globals
-int					*exit_status(void);
-char				***env_vars(void);
-// history
-void				clear_shell_history(void);
-void	handle_history(char *input);
-// path
-char				*search_path(const char *file);
-int					execute_external_command(char **args);
-// pipes
+// pipes.c
 int					execute_piped_commands(t_list *command_list);
-int					is_builtin(t_command *cmd);
-void				exec_builtin(t_command *cmd, int builtin);
-// pipes_utils
+
+// pipes_utils.c
 void				close_pipes(int pipe_fds[][2], int num_pipes);
 int					fork_and_execute(int pipe_fds[][2], t_list *current, int i,
 						int num_commands);
 int					wait_for_children(void);
 int					setup_child_pipes(int pipe_fds[][2], int i,
 						int num_commands);
-// redirection
+
+// redirection.c
 int					redirect_input(char *file);
 int					redirect_output(char *file, int append);
 int					handle_heredoc(char *delimiter);
 int					handle_redirections(t_list *command_list);
-// redirection utils
+
+// redirection_utils.c
 void				restore_std_fds(int orig_stdin, int orig_stdout);
 int					is_redirection(char *symbol);
 void				print_redirection_error(char *filename, char *error_msg);
 int					apply_single_redirection(char *symbol, char *filename);
 int					apply_redirections(t_command *cmd);
-// signals
+
+// ft_exit.c
+int					ft_exit(char *args[]);
+
+// Utility Functions
+
+// fake_globals.c
+int					*exit_status(void);
+char				***env_vars(void);
+int					*is_expanded(void);
+
+// history.c
+void				clear_shell_history(void);
+void				handle_history(char *input);
+
+// signal.c
 void				sigint_handler(int sig);
 void				handle_signals(void);
-// utils
-int					strcasecmp_custom(const char *s1, const char *s2);
+
+// utils.c
 void				free_tabel(void *content);
-// main
+
+// main.c
 int					execution(t_list *tabel);
-int					exec_new_shell(char **argv);
 void				main_loop(void);
 void				handle_shlvl(void);
-t_list				*parse_input(char *input);
-
 
 #endif
