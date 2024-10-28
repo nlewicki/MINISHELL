7b7ba78c2c4e94e82263c44a270bdcdfea968231
Nico <nlewicki@student.42heilbronn.de>
2024-10-28 16:20:39 +0100

    exit status change

--- src/execution/signal2.c
+++ src/execution/signal2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   signal2.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mhummel <mhummel@student.42.fr>            +#+  +:+       +#+        */
+/*   By: nlewicki <nlewicki@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/24 17:32:30 by mhummel           #+#    #+#             */
-/*   Updated: 2024/10/28 16:12:05 by mhummel          ###   ########.fr       */
+/*   Updated: 2024/10/28 16:20:28 by nlewicki         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void	heredoc_handler(int signum)
 	g_interrupt = 1;
 	rl_replace_line("", 0);
 	close(STDIN_FILENO);
-	*exit_status() = 130;
+	*exit_status() = 1;
 }
 
 void	ft_sigmode_heredoc(void)
